5304754e8231700ba29ffb9137c1295fd9effdc4
Sbusiso <you@example.com>
2022-07-01 10:33:22 -0700

    Betty warning

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,17 +6,18 @@
  *main -> assign a random number to the variable n each time it is executed code will check the condition   *and print output based on the condition
  *Return : always 0
  */
+
 int main(void)
 {
 int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if(n > 0)
+if (n > 0)
 	printf("%d is positive\n", n);
-if(n == 0)
+if (n == 0)
 	printf("%d is zero\n", n);
-if(n > 0)
+if (n > 0)
 	printf("%d is negative\n", n);
 return (0);
 }
