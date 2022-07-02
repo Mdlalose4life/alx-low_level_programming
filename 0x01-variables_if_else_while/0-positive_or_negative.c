a3fdce6b001c844b6fb3b3f03bf4e877c859825b
Sbusiso <you@example.com>
2022-07-02 06:30:20 -0700

    Betty permission

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,11 +3,9 @@
 #include <stdio.h>
 
  /**
-  * main - Entry point
-  *
-  * Return: Always 0 (Success)
+  * main - main block
+  * Return: 0
   */
-
 int main(void)
 {
 int n;
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (success)
+ */
+main(void)
+{
+	int ch;
+
+	for (ch = 'A' ; ch <= 'Z' ; Ch++)
+		putchar(ch);
+		putch('\n);
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+		putchar(ch);
+	return (0);
+}
