a09f5125d31f175cf2ec5599b2ff124ed479818b
Sbusiso <you@example.com>
2022-07-01 17:04:07 -0700

    wrong fing

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,7 +14,7 @@ srand(time(0));
 
 n = rand() - RAND_MAX / 2;
 if (n > 5)
-	printf("Last digit of %d is 8 and is greater than 5\n", n);
+	printf("Last digit of %d is 0 and os 0\n", n);
 if (n == 0)
 	printf("Last digit of %d is 0 and is 0\n", n);
 if (n < 6 && n != 0)
