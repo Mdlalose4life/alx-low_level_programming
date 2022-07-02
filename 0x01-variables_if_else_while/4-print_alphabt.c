cd8177dae1dd127d164c8371cf11ecf44ae658d2
Sbusiso <you@example.com>
2022-07-02 15:08:41 -0700

    The great betty

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,4 +1,18 @@
 #include <stdio.h>
+/**
+ * main - prints all the alphabets in lowercase, not e and q
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	char 
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+	if (ch != 'q' && ch != 'e')
+	putchar(ch);
+	}
+putchar('\n');
+return (0);
+}
