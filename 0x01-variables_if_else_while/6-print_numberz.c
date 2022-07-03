484ebb1bde2443e3188d6a827169796e7fb6d795
Sbusiso <you@example.com>
2022-07-02 17:02:58 -0700

    Second putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints digits from 0 to 10 followed by a new line.
+ * using a putchar.
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int numb = o;
+	for (; numb < 10 ; numb++)
+	{
+		putchar((numb % 10) + '0');
+	}
+	putchar('\n');
+	return (0);
+}
