f228040b88669bd39344a6eef99bf03dccd85ecd
Sbusiso <you@example.com>
2022-07-05 11:51:23 -0700

    betty move

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - This is a function that printd alphabets in lowercase
+ * followed by a new line.
+ * Return: 0.
+ */
+
+void print_alphabet(void);
+{
+char alh;
+	for (alph = 'a' ; alph <= 'z' ; alph++)
+	{
+		putcgar(alph);
+	}
+	putchar('\n')
+	retun(0);
+}
