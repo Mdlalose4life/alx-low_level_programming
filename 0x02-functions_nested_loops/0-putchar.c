5671a405fc3d436afeae4ba819f901e55ebcddd3
Sbusiso <you@example.com>
2022-07-05 07:13:08 -0700

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Printing _putchar and follow by a new line.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	printf("_putchar");
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+C-Functionns and nested loops
