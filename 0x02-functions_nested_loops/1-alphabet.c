f8449359169aa9d088312d87e90272cdd65882c7
Sbusiso <you@example.com>
2022-07-05 19:38:44 -0700

    void

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,6 @@
 /**
  * main - This is a function that printd alphabets in lowercase
  * followed by a new line.
- * Return: 0.
  */
 
 void print_alphabet(void)
@@ -13,5 +12,4 @@ char alph;
 		_putchar(alph);
 	}
 	_putchar('\n');
-	return (0);
 }
