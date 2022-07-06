6171401267122ede9ac00fe1efbe9eac8cf48c04
Sbusiso <you@example.com>
2022-07-06 12:42:34 -0700

    the table of 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *
+ * time_table - prints the table of 9, that start feom 0.
  *
  *
  */
@@ -9,8 +9,26 @@ void times_table(void);
 {
 int a;
 int b;
+int c;
+	for (a = 0 ; a <= 9 ; a++)
+	{
+		_putchar('0');
 
-	for
+		for (b = 1; b <= 9; b++)
+		{
+		_putchar(',');
+		_putchar(' ');
+
+		c = a * b;
+
+		if (c <= 9)
+			_putchar(' ');
+		else
+		_putchar((c / 10) + '0')
+		_putchar((c % 10) + '0');
+		}
+	_putchar('/n');
+	}
 
 
 }
