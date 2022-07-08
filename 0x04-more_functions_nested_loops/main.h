e4757bfce9778b78d9986490c48f49d157974ed4
Sbusiso <you@example.com>
2022-07-07 18:04:16 -0700

    edited a commer

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-int _putchar(char c)
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -9,4 +9,5 @@ void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
+
 #endif
