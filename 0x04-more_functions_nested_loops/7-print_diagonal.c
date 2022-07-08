664078492ba481b144708106bc7e77e7fac0ae8c
Sbusiso <you@example.com>
2022-07-08 07:13:18 -0700

    Second chance

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,10 +8,20 @@ void print_diagonal(int n)
 
 {
 	int a;
-	
-	if (a <= 0)
-		_putchar('\n');
-	for (a = 0; a <= n; a++)
-		_putchar(95);
+	int b;
+
+	if (n > 0)
+	{
+	for (a = 0; a < n; a++)
+	{
+	for (b = 0; b < a; b++)
+		_putchar(' ');
+	_putchar('\\');
+
+	if (a == n - 1)
+		continue;
+	_putchar('\n');
+	}
+	}
 	_putchar('\n');
 }
