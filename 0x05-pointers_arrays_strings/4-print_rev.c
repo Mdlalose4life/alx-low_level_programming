3392d3b64b93fd5971488f9164c54a03df7ec882
Sbusiso <you@example.com>
2022-07-11 15:09:23 -0700

    long fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,10 +11,13 @@ void print_rev(char *s)
 	int i;
 	int len;
 
-	for (len = 0; s[len] != '\0'; len++)
-		for (i = len - 1; i >= 0; i--)
-		{
-			_putchar(s[i]);
-		}
-			_putchar('\n');
+	len = 0;
+	while (s[len] != '\0')
+		len++;
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+		_putchar('\n');
 }
