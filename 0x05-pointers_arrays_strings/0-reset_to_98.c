9dd8ee6156df4d876b176e5f565eaaec3a04be0a
Sbusiso <you@example.com>
2022-07-11 06:07:50 -0700

    first commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reassign the value of pointer form *n = 402 to 8n = 98.
+ * @n: Original value of *n.
+ * Return: Uptadeted value of *n.
+ */
+
+void reset_to_98(int *n)
+{
+	return (*n = 98);
+}
