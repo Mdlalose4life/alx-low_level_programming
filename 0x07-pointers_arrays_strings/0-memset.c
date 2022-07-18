e4db885c51838b8c5df7d677d88dd3f1d6fbf077
Sbusiso <you@example.com>
2022-07-18 13:27:35 -0700

    second try

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -12,9 +12,8 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int i;
-unsigned int *dest = s;
-for (i = 0; dest < n; i++)
-	dest[i] = b;
+for (i = 0; i < n; i++)
+	s[i] = b;
 
 return (s);
 }
