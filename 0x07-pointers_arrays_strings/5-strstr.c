d8035ecca240ed3aeb49c8598abc21270e8d8d5e
Sbusiso <you@example.com>
2022-07-18 19:02:45 -0700

    best push

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -3,3 +3,22 @@
 
 /**
  *
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	
+	if (*needle == 0)
+		return 0;
+
+	for(; *haystack; heystack++)
+	{
+		i = 0;
+		
+		if (haystack[i] == needle[i])
+			do{
+				if (needle[i + 1] == '\0')
+					return (
+			}
+	}
+}
