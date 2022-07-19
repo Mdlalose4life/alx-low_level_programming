a272605a689bfc42e27a881685b4737b96b13db3
Sbusiso <you@example.com>
2022-07-19 15:23:54 -0700

    hot fix1

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,20 +10,18 @@
 void print_diagsums(int *a, int size)
 {
 
-int i = 0, s1 = 0, s2 = 0;
-	while (i < size)
+int i, s1 = 0, s2 = 0;
+	for (i = 0; i < size; i++)
 	{
 		s1 += a[i];
 		a += size;
-		i++;
 	}
 	a -= size;
 
-	while (i < size)
+	for (i = 0; i < size; i++)
 	{
 		s2 += a[i];
 		a -= size;
-		i++;
 	}
 	printf("%d, %d", s1, s2);
 }
