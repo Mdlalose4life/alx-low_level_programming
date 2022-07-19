5a40f71b8da28ba3a56e76db988bdd440ae11188
Sbusiso <you@example.com>
2022-07-18 19:09:28 -0700

    cold fix

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -11,8 +11,8 @@ void print_chessboard(char (*a)[8])
 
 	for (r = 0; a[r][7]; r++)
 	{
-	for (c = 0; c < 8; c++)
-		_putchar(a[r][c]);
+		for (c = 0; c < 8; c++)
+			_putchar(a[r][c]);
 
 		_putchar('\n');
 	}
