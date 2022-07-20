8ce285c5d7d161950908da1527201026df52d130
Sbusiso <you@example.com>
2022-07-20 16:42:38 -0700

    hope

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <math.h>
+/**
+ * _sqrt_recursion -> function that returns the natural
+ * square root of a number.
+ * @n: The Number whose roots will be retuned.
+ * Return: if n does not have root: fuction returns -1.
+ * If n does have roots: return the roots of n.
+ */
+
+int _sqrt_recursion(int n)
+{
+	int r = 0;
+
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 1)
+	{
+		return (1);
+	}
+	_sqrt_recursion(r);
+
+	return (n);
+}
