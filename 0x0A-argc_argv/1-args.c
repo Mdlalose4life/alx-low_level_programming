d728f674be573d99a326bfcf63912208bc6859a2
Sbusiso <you@example.com>
2022-07-22 19:25:04 -0700

    program that prints all arguments it receives.

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
  * main -  a program that prints the number of arguments passed into it.
- * argc: Arguments passed
- * argv: Pointer to the arguments provided.
+ * @argc: Arguments passed
+ * @argv: Pointer to the arguments provided.
  * Return: Always 0.
  */
-int main(int argc, char __attribute__((__unused__)), *argv[])
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
-	printf("%s\n", argv - 1);
-		return (0);
+	printf("%d\n", argc - 1);
+
+	return (0);
 }
