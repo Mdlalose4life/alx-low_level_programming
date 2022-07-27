f91db16ae97323925fe93c6da96723bb687cd851
Mdlalose4life <blessing1059@gmail.com>
2022-07-27 05:16:53 +0200

    hot fix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,13 +24,14 @@ for (l = 0; l < height; l++)
 a[l] = malloc(sizeof(int) * width);
 if (a[l] == NULL)
 {
-for (; b >= 0; b--)
+for (; l >= 0; l--)
 free(a[l]);
 return (NULL);
 }
 }
-for (b = 0; b < height; b++)
+for (l = 0; l < height; l++)
 {
+for (b = 0; b < wigth; b++)
 a[l][b] = 0;
 }
 return (a);
