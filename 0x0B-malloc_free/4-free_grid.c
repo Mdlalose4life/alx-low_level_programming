18e7161c2e59a1e5e4b61ef198286c32eb8aa762
Mdlalose4life <blessing1059@gmail.com>
2022-07-27 04:11:40 +0200

    function that frees a 2 dimensional of numners.

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,15 +1,15 @@
 #include "main.h"
-#include <stdlib.h>
 /**
  * free_grid -> function that frees a 2 dimensional of numners.
  * @grid: The array that must be freed.
- * @height: The height of a grid.
+ * @height: The height of a matrix.
+ * Return: Nothing
  */
 void free_grid(int **grid, int height)
 {
-int i = 0;
+int i;
 
-for (; i < height; i++)
+for (i = 0; i < height; i++)
 	free(grid[i]);
 free(grid);
 }
