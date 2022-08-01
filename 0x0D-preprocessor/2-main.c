77f3df6e2079687ba5860147f4664b7377f9eeda
Mdlalose4life <blessing1059@gmail.com>
2022-08-01 18:58:27 +0200

    prints the name of the file it was compiled from, followed by a new line.

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,13 @@
-#ifndef MAIN_H
-#define MAIN_H
 
-#define __FILE__
+#include "main.h"
+#include <stdio.h>
 
-#endif
+/**
+ * main - prints the name of the folder it's currently occupied in.
+ * Return: always 0.
+ */
+
+int main()
+{
+printf("%s", __FILE__);
+}
