bc057c47945072a0018a8a73ab68dea8708013de
Mdlalose4life <blessing1059@gmail.com>
2022-08-02 03:48:21 +0200

    hot fix

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,7 +11,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-*name = &Poppy;
-age = 3.5;
-*owner = &Bob;
+if ( d != NULL)
+{
+	d.(*name) = name;
+	d.(*age) = age;
+	d.(*owner) = owner;
+}
 }
