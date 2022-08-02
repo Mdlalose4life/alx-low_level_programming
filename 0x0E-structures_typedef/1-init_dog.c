f660a0a41ccff5f451220fad82436347b98a83c7
Mdlalose4life <blessing1059@gmail.com>
2022-08-03 00:07:38 +0200

    hot fix

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,14 +8,13 @@
  * @name: name of a dog.
  * @age: The age of a dog.
  * @owner: The owner of a dog.
- * Return: Always 0.
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
 	{
 		d->name = name;
-		d->age = 3.5;
+		d->age = age;
 		d->owner = owner;
 	}
 }
