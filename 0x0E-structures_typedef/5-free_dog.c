1034b6e28150a41290b4b47c37f68e0e6e124d55
Mdlalose4life <blessing1059@gmail.com>
2022-08-03 00:56:43 +0200

    betty compliant

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -5,11 +5,11 @@
  * free_dog - frees memory of a structure.
  * @d: The structure to be freed.
  */
- void free_dog(dog_t *d)
- {
- 	if (d == NULL)
- 		return;
- 	free(d->owner);
- 	free(d->name);
- 	free(d);
- }
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->owner);
+	ree(d->name);
+	free(d);
+}
