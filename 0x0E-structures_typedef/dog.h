1383e6a8779d9bf8ce9fe00de1589a425a60cf57
Mdlalose4life <blessing1059@gmail.com>
2022-08-02 02:22:29 +0200

    the dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-{struct
+/**
+* struct dog - Name of the new structure.
+* @name: Name of a dog
+* @age: Age of a dog
+* @owner: owner of a dog
+*/
+struct dog
+{
 char *name;
 float age;
 char *owner;
