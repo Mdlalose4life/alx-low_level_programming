3c88efba9ce594e470d23c125c31de15669b6b68
Mdlalose4life <blessing1059@gmail.com>
2022-08-04 01:29:43 +0200

    A for loop

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,7 +1,7 @@
 #include <stddef.h>
 #include "function_pointers.h"
 /**
- * int_index -> Function that searches for an interger,
+ * int_index -> Function that searches for an interger.
  * @array: An array
  * @size: A size of an array.
  * @cmp: A pointer to a function that needs to be searched.
@@ -12,14 +12,13 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-int i = 0;
+int i;
 	if (array == NULL || cmp == NULL)
 		return (-1);
-	while (i < size)
+	for (; i < size; i++)
 	{
 	if (cmp(array[i]) != 0)
 		return (i);
-		i++;
 	}
 	return (-1);
 }
