0825a86d49e94d0cb4abc4215728adc91a598c22
Mdlalose4life <blessing1059@gmail.com>
2022-08-05 06:31:04 +0200

    Readme.md

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_all - A function that prints anything.
+ * @format: List of types of arguments passed.
+ * Return: 0 (success)
+ */
+void print_all(const char * const format, ...)
+{
+
+}
