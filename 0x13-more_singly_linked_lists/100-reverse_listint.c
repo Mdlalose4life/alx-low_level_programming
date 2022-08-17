f99d7d80bd05f2d643bd11e5559310b21bb90aac
Mdlalose4life <blessing1059@gmail.com>
2022-08-17 05:37:34 +0200

    satisfied betty

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -2,13 +2,13 @@
 /**
  * reverse_listint -> reverses a listint_t linked list.
  * @head: The pointer to the head of the address of the head.
- * return: pointer to the first node of the reversed list.
+ * Return: pointer to the first node of the reversed list.
  */
 listint_t *reverse_listint(listint_t **head)
 {
 listint_t *start, *end;
 if (head == NULL || *head == NULL)
-	return(NULL);
+	return (NULL);
 end = NULL;
 while ((*head)->next != NULL)
 {
