470bcee2cad0d94240faddb9d55ba71115052a94
Mdlalose4life <blessing1059@gmail.com>
2022-08-17 02:10:24 +0200

    modifications

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,15 +1,16 @@
 #include "lists.h"
+#include <stdlib.h>
 /**
  * free_listint -> frees a list.
  * @head: A pointert to the head of the list.
  */
 void free_listint(listint_t *head)
 {
-listint_t *ptr
-while head;
+listint_t *ptr;
+while (head)
 {
-	ptr = head->next
-	free(head)
+	ptr = head->next;
+	free(head);
 	head = ptr;
 }
 }
