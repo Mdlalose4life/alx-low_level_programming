473ad2c5206e604dacf03543ef38dad8dc875b49
Mdlalose4life <blessing1059@gmail.com>
2022-08-19 02:16:37 +0200

    sets the value of a bit to 1 at a given index.

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * get_bit - sets the value of a bit to 1 at a given index.
+ * @index: index at the value to be obtained: starts from 0.
+ * @n: The bit.
+ * Return: returns -1 if an error occured.
+ * otherwise: Return 1
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+if (index >= (sizeof(n) * 8))
+	return (-1);
+	n ^= (1 << index);
+	return (1);
+}
--- 0x14-bit_manipulation/README.md
+++ 0x14-bit_manipulation/README.md
@@ -1,4 +1,5 @@
 #This is readme for bit_manipulation project.
-1. A function that converts a binary number to an unsigned int.
+0. A function that converts a binary number to an unsigned int.
+1. A function that prints the binary representation of a number.
 2. A function that returns the value of a bit at a given index.
-
+3. A function that sets the value of a bit to 1 at a given index.
