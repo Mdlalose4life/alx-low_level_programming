04d35b78c85d70f771ef15e21fa3f6560be6f073
Mdlalose4life <blessing1059@gmail.com>
2022-08-23 03:59:19 +0200

    hot fix

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,7 +12,7 @@ int append_text_to_file(const char *filename, char *text_content)
 int x, y, length = 0;
 if (filename == NULL)
 return (-1);
-if (text_content == NULL)
+if (text_content != NULL)
 {
 	for (length = 0; text_content[length];)
 	length++;
