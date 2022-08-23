0a9540be66b9e617bd98b535188b94a507594884
Mdlalose4life <blessing1059@gmail.com>
2022-08-23 02:36:11 +0200

    addded all the files

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ * read_textfile -  reads a text file and prints
+ * it to the POSIX standard output.
+ * @filename: The pointer to name of the file.
+ * @letters: latters to be read out.
+ * Return: if file cannot open return 0.
+ * if file is NULL return 0.
+ * if write failes: Return 0
+ * otherwise: return the number of latters
+ * it reads and prints.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+ssize_t x, y, z;
+char *buffers;
+if (filename == NULL)
+	return (0);
+buffers = malloc(sizeof(char) * letters);
+if (buffers == NULL)
+	return (0);
+x = open(filename, O_RDONLY);
+y = read(x, buffers, letters);
+z = write(STDOUT_FILENO, buffers, y);
+if (x == -1 || y == -1 || z == -1 || z != y)
+{
+	free(buffers);
+	close(0);
+}
+return (z);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,5 @@
+This is reame for the C - File I/O
+1. A function that reads a text file and prints it to the POSIX standard output.
+2. A function that creates a file.
+3. A function that appends text at the end of a file
+4. A program that copies the content of a file to another file.
