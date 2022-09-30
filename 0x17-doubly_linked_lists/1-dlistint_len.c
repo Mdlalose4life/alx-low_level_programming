12cd289e4648469aeac780e6efb9681a9919f56c
Mdlalose4life <blessing1059@gmail.com>
2022-09-30 02:19:16 +0200

    Done

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * dlistint_len - counts the number of nodes in dlistint_t.
+ * @h: The pointer to the head of a structure.
+ * Return: The number of nodes on the struct dlistint_t.
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+size_t i;
+for (i = 0; h != NULL; i++)
+	h = h->next
+return (i)
+}
---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * dlistint_t *add_dnodeint - Adds the node at the begganing of a list.
+ * @head: Double pointer to the heard of a structure
+ * @n: value to be added with a new node. 
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+char *temp;
+
+}
