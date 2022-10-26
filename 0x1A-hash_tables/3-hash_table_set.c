1080caed92c50b1554e5732984c1b56245fe877d
Mdlalose4life <blessing1059@gmail.com>
2022-10-27 01:30:50 +0200

    Hash node

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,38 +1,73 @@
 #include "hash_tables.h"
+
 /**
- * hash_table_set - adds an element to the hash table
- * @ht: hash table to add or update the key/value to.
- * @key: The key
- * @value: The value
- * Return: 1 if it succeeded, 0 otherwise
+ * make_hash_node - creating a new hash node
+ * @key: key for the node
+ * @value: for the node
+ *
+ * Return: the new node, or NULL on failure
+ */
+hash_node_t *make_hash_node(const char *key, const char *value)
+{
+	hash_node_t *new_node;
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (NULL);
+	}
+	new_node->next = NULL;
+	return (new_node);
+}
+
+
+/**
+ * hash_table_set - sets a key to a value(iterm) in the hash table
+ * @ht: hash table to add elemt to
+ * @key: key for the data
+ * @value: data to store
+ *
+ * Return: 1 if successful, 0 otherwise
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-hash_node_t *i, *new_item;
-size_t index;
-char *key_copy, *value_copy;
-if (!key || !value || !ht)
-	return (0);
-new_item = malloc(sizeof(hash_node_t));
-if (!new_item)
-	return (0);
-index = key_index((const unsigned char *)key, ht->size)
-key_copy = strdup(key)
-value_copy = strdup(value)
-for (i = ht->array[index]; i != NULL; i = i->next)
-{
-	if (strcmp(i->key, key) == 0)
+	unsigned long int index;
+	hash_node_t *hash_node, *ptr;
+	char *new_value;
+
+	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
+	    key == NULL || strlen(key) == 0 || value == NULL)
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	ptr = ht->array[index];
+	while (ptr != NULL)
 	{
-		if (i->value)
-			free(i->value);
-		i->value = value_copy;
-		free(new_item);
-		return (1);
+		if (strcmp(ptr->key, key) == 0)
+		{
+			new_value = strdup(value);
+			if (new_value == NULL)
+				return (0);
+			free(ptr->value);
+			ptr->value = new_value;
+			return (1);
+		}
+		ptr = ptr->next;
 	}
-}
-new_item->key = key_copy;
-new_item->value = value_copy;
-new_item->next = ht->array[index];
-ht->array[index] = new_item;
-return (1);
+	hash_node = make_hash_node(key, value);
+	if (hash_node == NULL)
+		return (0);
+	hash_node->next = ht->array[index];
+	ht->array[index] = hash_node;
+	return (1);
 }
