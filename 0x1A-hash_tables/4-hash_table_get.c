b0e9059b98f9000c1e7b2ca1e397686e920c535a
Mdlalose4life <blessing1059@gmail.com>
2022-10-27 01:37:03 +0200

    Hash Table

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - seaches a value associated with a key
+ * @ht: table to seach value from
+ * @key: key to find value
+ *
+ * Return: value associated with key, or NULL if key cannot be found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *ptr;
+
+	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
+	    key == NULL || strlen(key) == 0)
+		return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+	ptr = ht->array[index];
+	while (ptr != NULL)
+	{
+		if (strcmp(ptr->key, key) == 0)
+			return (ptr->value);
+		ptr = ptr->next;
+	}
+	return (NULL);
+}
