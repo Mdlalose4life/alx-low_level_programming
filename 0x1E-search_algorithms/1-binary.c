0e736f859da65087003abe58a4a79b59809d2ab8
Mdlalose@1104 <blessing1059@gmail.com>
2023-01-30 02:47:03 +0200

    Changing the sign

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,22 +1,26 @@
 #include "search_algos.h"
+#include <stdio.h>
 /**
- *
- *
- *
- *
- *
- *
+ * binary_search - The Function to perfom binary serch
+ * in an array.
+ * @array: An array to be searched
+ * @size: size of an array to be searched
+ * @value: The value to search for in an array
+ * Return: retuns the searched value or -1 if
+ * none is found.
  */
 int binary_search(int *array, size_t size, int value)
 {
 	int lower, mid, higher;
 	int i;
-	lower = 0;
-	higher = size + 1;
+	
 	if (array == NULL)
 	{
 		return (-1);
 	}
+	
+	higher = size - 1;
+	lower = 0;
 	
 	while(lower <= higher)
 	{
@@ -25,14 +29,11 @@ int binary_search(int *array, size_t size, int value)
 		printf("Searching in array: ");
 		for (i = lower; i <= higher; i++)
 			printf("%i%s", array[i], i == higher ? "\n": ", ");
-		
-		if (array[mid] == value)
-			return mid;
-		
+
 		if (array[mid] < value)
 			lower = mid + 1;
 		else if (array[mid] > value)
-			higher = mid -1;
+			higher = mid - 1;
 		else
 			return (mid);
 	}
